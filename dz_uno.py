45084e5e0af7a482535fb41bea7808f3aba8a896
Maria <marykor1997@gmail.com>
2022-09-04 15:47:42 +0300

    Added task 2

--- dz_uno.py
+++ dz_uno.py
@@ -6,3 +6,23 @@ elif 6 <= number <= 7:
     print ("Выходной :)")
 else:
     print ("Нет дня недели с таким номером")
+    
+    
+    
+# Программа, которая принимает на вход координаты точки (X и Y), причём X ≠ 0 и Y ≠ 0 и выдаёт номер четверти плоскости, в которой находится эта точка (или на какой оси она находится). 
+coordinate_x = int(input("x = "))
+coordinate_y = int(input ("y = "))
+if coordinate_x > 0 and coordinate_y > 0:
+    print ("Точка - в 1 четверти")
+elif coordinate_x < 0 and coordinate_y > 0:
+    print ("Точка - во 2 четверти")
+elif coordinate_x < 0 and coordinate_y < 0:
+    print ("Точка - в 3 четверти")
+elif coordinate_x > 0 and coordinate_y < 0:
+    print ("Точка - в 4 четверти")
+elif coordinate_x == 0 and (coordinate_y > 0 or coordinate_y < 0):
+    print ("Точка - на оси y")
+elif (coordinate_x > 0 or coordinate_x < 0) and coordinate_y == 0:
+    print ("Точка - на оси x")
+else:
+    print ("Точка -это место пересечения осей x и y")
