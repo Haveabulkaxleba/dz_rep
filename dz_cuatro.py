8a9fb4dc4b51164269fff5f3564c97f920042968
Maria <marykor1997@gmail.com>
2022-09-16 22:09:06 +0300

    Умерла, но сделала

--- dz_cuatro.py
+++ dz_cuatro.py
@@ -31,7 +31,7 @@ def fac (a):
     return lst
 print (fac(numberrr))
 
-#Задача 3. Задайте последовательность чисел. 
+# Задача 3. Задайте последовательность чисел. 
 # Напишите программу, которая выведет список неповторяющихся элементов исходной последовательности.
 number = input('Введите последовательность чисел: ').split()
 def non_repeat (n):
@@ -50,17 +50,17 @@ lst = []
 def my_func(k):
     global lst
     for i in range (0, 3):
-        coef = random.randint(0, 100)
+        coef = random.ransdint(0, 100)
         lst.append(coef)
     if lst[0] != 0:
         if lst[1] == 0 and lst[2] != 0:
-            result = ('{}x^{} + {} = 0'.format(lst[0], k, lst[2]))
+            result = ('{}x^{} + {}'.format(lst[0], k, lst[2]))
         elif lst[2] == 0 and lst[1] != 0:
-            result = ('{}x^{} + {} = 0'.format(lst[0], k, lst[1]))
+            result = ('{}x^{} + {}'.format(lst[0], k, lst[1]))
         elif lst[1]== 0 and lst[2] == 0:
-            result = ('{}x^{} = 0'.format(lst[0], k, lst[2]))
+            result = ('{}x^{}'.format(lst[0], k, lst[2]))
         else:
-            result = '{}x^{} + {}x + {} = 0'.format(lst[0], k,  lst[1], lst[2])
+            result = '{}x^{} + {}x + {}'.format(lst[0], k,  lst[1], lst[2])
         return result
     else:
         return "Try again"
@@ -70,3 +70,75 @@ print (m)
 f = open('my_file.txt', 'w')
 f.write(m)
 f.close()
+
+# Задача 5. Даны два файла, в каждом из которых находится запись многочлена. Задача - сформировать файл, содержащий сумму многочленов.
+with open('my_file2.txt', 'w') as f_new:
+    f_new.write('2x^2 + 4x + 5')
+f_new = open('my_file2.txt', 'r')
+read = f_new.read()
+f_new.close()
+
+f_old = open ('my_file.txt', 'r') #Открываем файл из предыдущего задания
+read1 = f_old.read()
+f_old.close()
+
+def get_ones(line):
+    tmp = []
+    last = 0
+    positive = True
+    for i, item in enumerate(line):
+        if item in {'+', '-'}:
+            if positive:
+                tmp.append(line[last:i])
+            else:
+                tmp.append('-' + line[last:i])
+            last = i + 1
+            positive = item == '+'
+ 
+    if positive:
+        tmp.append(line[last:])
+    else:
+        tmp.append('-' + line[last:])
+    return tmp
+ 
+ 
+def get_coef(one):
+    for i, item in enumerate(one):
+        if item == 'x':
+            return int(one[:i]), one[i:]
+    return int(one), None
+ 
+ 
+lst1 = get_ones(read.replace(' ', '').replace('*', ''))
+lst2 = get_ones(read1.replace(' ', '').replace('*', ''))
+ 
+dct1 = {item[1]: item[0] for item in map(get_coef, lst1)}
+dct2 = {item[1]: item[0] for item in map(get_coef, lst2)}
+
+ 
+print(dct1)
+print(dct2)
+
+lst1 = list(dct1.values())
+lst2 = list(dct2.values())
+print(lst1)
+print (lst2)
+
+for i in lst1:
+    lst3 = []
+    n = 0
+    for k in lst2:
+        k = 0
+        b = lst1[n] + lst2[k]
+        lst3.append(b)
+        n += 1
+        k += 1
+print (lst3)
+
+# Записываем сумму многочленов в новый файл
+with open('final_file.txt', 'w') as another_f:
+    another_f.write('{}x^2 + {} + {}'. format(lst3[0], lst3[1], lst3[2]))
+
+
+
+        
